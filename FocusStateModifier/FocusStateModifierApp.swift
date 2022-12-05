deac47e7622e9c570d54b6d2bf565ff12cedf739
seanmrich <sean.rich.register@mac.com>
2022-12-05 14:24:30 -0500

    Initial Commit

---
+++ FocusStateModifier/FocusStateModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusStateModifierApp.swift
+//  FocusStateModifier
+//
+//  Created by Sean Rich on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FocusStateModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
